77c1c287ec9963a9695d397b1b00755e85724586
Alexander <alexanderneto5@gmail.com>
2019-09-11 10:39:52 +0100

    Consume of API-REST de Petclinic creating a component list-owners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-//import { SearchModule } from './search/search.module';
+import { SearchModule } from './search/search.module';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,12 +9,11 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,    
+    SearchModule  
   ],
   
   providers: [],
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormSearchComponent } from './form-search/form-search.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ListOwnersComponent } from '../list-owners/list-owners.component';
+import { OwnerService } from './owner.service';
 
 
 
 @NgModule({
   imports: [
-    CommonModule
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
   ],
   exports: [
-    FormSearchComponent
+    FormSearchComponent, ListOwnersComponent
   ],
-  declarations: [FormSearchComponent],
+  declarations: [FormSearchComponent, ListOwnersComponent],
+  providers: [OwnerService],
 })
 export class SearchModule { }
